0ebc2cef3b72226f00d04306bbcbf5d5f96c57df
jwhan77 <gksjeongw96@gmail.com>
2022-02-02 23:28:02 +0900

    Update intro

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import webdev from "./undraw_proud_coder_re_exuy.svg";
+
 import "./Main.css";
 
 const Main = () => {
@@ -8,17 +10,23 @@ const Main = () => {
       <div className="main-intro">
         <div className="name">Jungwon Han</div>
         <div className="txt-1">
-          <p>Front-end developer</p>
-          <p>based in South Korea</p>
+          <p>Front end developer</p>
+          <p>based in Korea</p>
         </div>
         <div className="txt-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
-        <div className="button-orange">SEE MY WORK</div>
+        <div
+          className="button-orange"
+          onClick={() => (window.location.href = "#work")}
+        >
+          SEE MY WORK
+        </div>
       </div>
       <div className="main-images">
-        <div>Image</div>
+        <div className="bg-oval"></div>
+        <img src={webdev} alt="" className="developer" />
       </div>
     </div>
   );
