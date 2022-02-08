f3b7e9bf349d7cf716d7038cc92e23bd90027666
jwhan77 <gksjeongw96@gmail.com>
2022-02-08 22:17:27 +0900

    Responsive form

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -6,17 +6,19 @@ import "./Work.css";
 
 const Work = () => {
   return (
-    <div className="latest-work">
-      <div className="name">PORTFOLIO</div>
-      <div className="work-title">
-        <div className="txt-1">Latest Work</div>
-        <div>
-          <button className="button-transparent">
-            <span>EXPLORE MORE</span>
-          </button>
+    <div className="work-container">
+      <div className="latest-work">
+        <div className="name">PORTFOLIO</div>
+        <div className="work-title">
+          <div className="txt-1">Latest Work</div>
+          <div>
+            <button className="button-transparent">
+              <span>EXPLORE MORE</span>
+            </button>
+          </div>
         </div>
+        <div className="content">{/* <Slider /> */}</div>
       </div>
-      <div className="content">{/* <Slider /> */}</div>
     </div>
   );
 };
